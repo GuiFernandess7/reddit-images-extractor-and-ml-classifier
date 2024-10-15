752b07b0bcc1c5583046d3c5598af92c6ff7f483
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-10-15 17:48:17 -0300

    subreddit column added

--- app/db/entities/UserImages.py
+++ app/db/entities/UserImages.py
@@ -16,7 +16,8 @@ class UserImages(Base):
     image_url = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
     sex = Column(Enum(Gender), nullable=True)
+    subreddit = Column(String, nullable=True)
 
     def __repr__(self):
         return (f"<UserImages(ts={self.ts}, image_url='{self.image_url}', "
-                f"created_at={self.created_at}, sex='{self.sex}')>")
+                f"created_at={self.created_at}, sex='{self.sex}')>, subreddit='{self.subreddit}')>")
